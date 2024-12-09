553003de002462e7c150d71fa0ecdc9dd790d205
Shishuta Nazar <131581944+Ultra416@users.noreply.github.com>
2024-12-09 10:27:10 +0200

    Update Laba4.py

--- Laba4.py
+++ Laba4.py
@@ -6,11 +6,6 @@ TOKEN = "8172214939:AAE-If9yql9uCfG6wEvvqVyG3cFbC54TCiU"
 # створення бота
 bot = telebot.TeleBot(TOKEN)
 
-# початок взаємодії командою /start
-@bot.message_handler(commands=['start'])
-def send_welcome(message):
-    bot.reply_to(message, "Привіт! Я ехо-бот. Напиши мені щось, і я повторю!")
-
 # повторює Кожне повідомлення 
 @bot.message_handler(func = lambda message: True)
 def echo_all(message):
